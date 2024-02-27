4188db2e048c58e0411d56c9dfb45cfa78ee12d5
lanjerry <38046851@qq.com>
2024-02-27 19:17:56 +0800

    feat: 新增sharding-jdbc水平分片新增分布式序列配置.

--- sharding-jdbc-demo/src/main/java/com/lanjerry/shardingjdbcdemo/entity/Order.java
+++ sharding-jdbc-demo/src/main/java/com/lanjerry/shardingjdbcdemo/entity/Order.java
@@ -19,7 +19,9 @@ import java.math.BigDecimal;
 @TableName("t_order")
 public class Order {
 
-    @TableId(type = IdType.ASSIGN_ID)
+    // 当配置了shardingsphere-jdbc的分布式序列时，自动使用shardingsphere-jdbc的分布式序列
+    // 当没有配置shardingsphere-jdbc的分布式序列时，自动依赖数据库的主键自增策略
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String orderNo;
     private Long userId;
